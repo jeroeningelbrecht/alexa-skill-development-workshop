b0383ad801a95cd488013dc55172ab8c56e25148
Jeroen Ingelbrecht <jeroen.ingelbrecht@kbc.be>
2017-06-02 14:37:54 +0200

    update

--- src/app.js
+++ src/app.js
@@ -1,7 +1,35 @@
-const WELCOME_MESSAGE = 'Hello World!';
+const WELCOME_MESSAGE = 'Welcome to High Low guessing game.  You have played 0 times.  Would you like to play?';
+const GREAT = 'Great! Try saying a number to start the game';
+const TOO_LOW = ' is too low';
+const TOO_HIGH = ' is too high';
+const CORRECT = ' is correct! Would you like to play a new game?';
+
+let speech = '';
+
+const Alexa = require('alexa-sdk');
+
+const states = {
+    START: '_START',
+    GUESSING: '_GUESSING',
+};
+
 
 module.exports.handlers = {
     LaunchRequest() {
-        this.emit(':tell', WELCOME_MESSAGE);
+    	this.handler.state = states.START;
+        this.emitWithState('Start');
+    },
+    Unhandled() {
+        this.handler.state = states.START;
+        this.emitWithState('Start');
     }
+    
 };
+
+module.exports.starthandlers = Alexa.createStateHandler(states.START, {
+
+	Start(){
+		this.emit(':ask', WELCOME_MESSAGE);
+	}
+
+});
