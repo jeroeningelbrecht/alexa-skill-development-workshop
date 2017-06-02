bffde4b87cb3135dc5e0d673440979df3b01baae
Jeroen Ingelbrecht <jeroen.ingelbrecht@kbc.be>
2017-06-02 15:12:20 +0200

    update

--- src/app.js
+++ src/app.js
@@ -57,7 +57,7 @@ module.exports.guesshandlers = Alexa.CreateStateHandler(states.GUESSING, {
 	'NumberGuessIntent' : function() {
 		var number = parseInt(this.event.request.intent.slots.number.value);
 		if(number === this.attributes.numberToGuess) {
-			this.emit(':tell', CORRECT);
+			this.emit(':ask', CORRECT);
 		}
 	},
 
